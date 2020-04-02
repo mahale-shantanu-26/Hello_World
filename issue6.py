cf75b2bb8b573961f417cd5c303e2f93ae0bd3fb
Shantanu <mahaleshantanu4@gmail.com>
2020-04-02 16:07:29 +0530

    Revert "Create issue6.py"
    This reverts commit 1930f2f49fa34cdefb3704209682786c49c45b85.

--- issue6.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-message = """<<html><head>
-</head><body><p>Hello world</p></body>
-</html>"""
-f = open('helloworld.html', 'w')
-f.write(message)
-f.close()
-webbrowser.open("file:///home/suryaveer/.config/spyder-py3/helloworld.html")
